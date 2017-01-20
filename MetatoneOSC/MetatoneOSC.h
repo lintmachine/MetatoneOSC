2774cfb84c8683d2856703bfa481b7f8d68baf91
cdann <cdann@lintmachine.com>
2017-01-19 23:49:18 -0800

    Add framework project for Cartage dependency management support.

---
+++ MetatoneOSC/MetatoneOSC.h
@@ -0,0 +1,18 @@
+//
+//  MetatoneOSC.h
+//  MetatoneOSC
+//
+//  Created by cdann on 1/19/17.
+//  Copyright © 2017 lintmachine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MetatoneOSC.
+FOUNDATION_EXPORT double MetatoneOSCVersionNumber;
+
+//! Project version string for MetatoneOSC.
+FOUNDATION_EXPORT const unsigned char MetatoneOSCVersionString[];
+
+#import <MetatoneOSC/F53OSC.h>
+
